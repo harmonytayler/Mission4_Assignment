8e83a9b60bac6cd3c63aa9f79a96c0286e283e79
Harmony Tayler <153692183+harmonytayler@users.noreply.github.com>
2025-01-29 13:46:36 -0700

    Fixed everything with magic

--- Gameplay.cs
+++ Gameplay.cs
@@ -11,7 +11,7 @@ namespace Mission4_Assignment
         // Receive the "board" array from the driver (Program) class.
 
         // Contain a method that prints the board based on the information passed to it.
-        public string PrintBoard(string[[]] board)
+        public void PrintBoard(string[,] board)
         {
             Console.WriteLine("  {0}  |  {1}  |  {2}  ", board[0, 0], board[0, 1], board[0, 2]);
             Console.WriteLine("-----|-----|-----");
@@ -20,16 +20,16 @@ namespace Mission4_Assignment
             Console.WriteLine("  {0}  |  {1}  |  {2}  ", board[2, 0], board[2, 1], board[2, 2]);
         }
 
-        public string CheckForWinner(string[[]] board)
+        public bool CheckForWinner(string[,] board)
         {
             bool gameOver = false;
             int spacesFilled = 0;
 
-            for (int i = 0;i < 3; i++)
+            for (int i = 0; i < 3; i++)
             {
                 if (board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2])
                 {
-                    if (board[i,0] == "O")
+                    if (board[i, 0] == "O")
                     {
                         Console.Write("Player 1 is the winner!");
                         gameOver = true;
@@ -57,65 +57,57 @@ namespace Mission4_Assignment
                         return gameOver;
                     }
                 }
-                else if (board[0,0] == board[1,1] && board[1,1] == board[2,2])
+            }
+
+            if (board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2])
+            {
+                if (board[0, 0] == "O")
                 {
-                    if board[0,0] == "O"
-                    {
-                        Console.Write("Player 1 is the winner! >:)");
-                        gameOver = true;
-                        return gameOver;
-                    }
-                    else
-                    {
-                        Console.Write("Player 2 is the winner! >:)");
-                        gameOver = true;
-                        return gameOver;
-                    }
-                else if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
-                    {
-                        if board[0, 2] == "O"
-                        {
-                            Console.Write("Player 1 is the winner! >:)");
-                            gameOver = true;
-                            return gameOver;
-                        }
-                        else
-                        {
-                            Console.Write("Player 2 is the winner! >:)");
-                            gameOver = true;
-                            return gameOver;
-                        }
-                    }
-                    else if
-                        {
-                        for (int i = 0; i < 3; i++)
-                        {
-                            for (int j = 0; j < 3; j++)
-                            {
-                                if (board[i, j] == "O" || board[i, j] == "X")
-                                {
-                                    spacesFilled++;
-                                }
-                                if (spacesFilled >= 9)
-                                {
-                                    Console.WriteLine("It's a tie?! :|")
-                                    gameOver = true;
-                                    return gameOver;
-                                }
-
-                            }
-                        }
-                    }
-                    else 
-                    {
-                        return gameOver;
-                    } 
+                    Console.Write("Player 1 is the winner! >:)");
+                    gameOver = true;
+                    return gameOver;
                 }
+                else
+                {
+                    Console.Write("Player 2 is the winner! >:)");
+                    gameOver = true;
+                    return gameOver;
+                }
+            }
+            else if (board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0])
+            {
+                if (board[0, 2] == "O")
+                {
+                    Console.Write("Player 1 is the winner! >:)");
+                    gameOver = true;
+                    return gameOver;
+                }
+                else
+                {
+                    Console.Write("Player 2 is the winner! >:)");
+                    gameOver = true;
+                    return gameOver;
+                }
+            }
 
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if (board[i, j] == "O" || board[i, j] == "X")
+                    {
+                        spacesFilled++;
+                    }
+                }
+            }
+
+            if (spacesFilled >= 9)
+            {
+                Console.WriteLine("It's a tie?! :|");
+                gameOver = true;
+            }
+
+            return gameOver;
         }
-
-
-
-        // Contain a method that receives the game board array as input and returns if there is a winner and who it was.
     }
 }
