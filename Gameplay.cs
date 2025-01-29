c4af499be09db5c94ee1a89603e75819efcf895a
Harmony Tayler <153692183+harmonytayler@users.noreply.github.com>
2025-01-29 15:35:54 -0700

    Added comments and fixed input error handling to handle non-integer inputs.

--- Gameplay.cs
+++ Gameplay.cs
@@ -8,11 +8,10 @@ namespace Mission4_Assignment
 {
     internal class Gameplay
     {
-        // Receive the "board" array from the driver (Program) class.
-
-        // Contain a method that prints the board based on the information passed to it.
+        //Receives the array of moves and returns nothing because it just prints the whole board in the method.
         public void PrintBoard(string[,] board)
         {
+            //Prints the board in a grid based on the contents of the moves array.
             Console.WriteLine("  {0}  |  {1}  |  {2}  ", board[0, 0], board[0, 1], board[0, 2]);
             Console.WriteLine("-----|-----|-----");
             Console.WriteLine("  {0}  |  {1}  |  {2}  ", board[1, 0], board[1, 1], board[1, 2]);
@@ -20,68 +19,41 @@ namespace Mission4_Assignment
             Console.WriteLine("  {0}  |  {1}  |  {2}  ", board[2, 0], board[2, 1], board[2, 2]);
         }
 
+        //Receives the array of moves and checks for a winner, then returns a bool regarding whether the game is over.
         public bool CheckForWinner(string[,] board)
         {
             bool gameOver = false;
-            int spacesFilled = 0;
 
+            //Checks rows and columns for a win and switches gameOver to true if there is one.
             for (int i = 0; i < 3; i++)
             {
+                //Checks each row for a win.
                 if ((board[i, 0] == board[i, 1] && board[i, 1] == board[i, 2]) && board[i,0] != " ")
                 {
-                    if (board[i, 0] == "O")
-                    {
-                        gameOver = true;
-                        return gameOver;
-                    }
-                    else
-                    {
-                        gameOver = true;
-                        return gameOver;
-                    }
+                    gameOver = true;
+                    return gameOver;
                 }
+                //Checks each column for a win.
                 else if ((board[0, i] == board[1, i] && board[1, i] == board[2, i]) && board[0, i] != " ")
                 {
-                    if (board[0, i] == "O")
-                    {
-                        gameOver = true;
-                        return gameOver;
-                    }
-                    else
-                    {
-                        gameOver = true;
-                        return gameOver;
-                    }
+                    gameOver = true;
+                    return gameOver;
                 }
             }
 
+            //Checks each diagonal for a win and switches gameOver to true if there is one.
             if ((board[0, 0] == board[1, 1] && board[1, 1] == board[2, 2]) && board[0, 0] != " ")
             {
-                if (board[0, 0] == "O")
-                {
-                    gameOver = true;
-                    return gameOver;
-                }
-                else
-                {
-                    gameOver = true;
-                    return gameOver;
-                }
+                gameOver = true;
+                return gameOver;
             }
             else if ((board[0, 2] == board[1, 1] && board[1, 1] == board[2, 0]) && board[0, 2] != " ")
             {
-                if (board[0, 2] == "O")
-                {
-                    gameOver = true;
-                    return gameOver;
-                }
-                else
-                {
-                    gameOver = true;
-                    return gameOver;
-                }
+                gameOver = true;
+                return gameOver;
             }
 
+            //If there was no win, gameOver will continue to be false.
             return gameOver;
         }
     }
--- Program.cs
+++ Program.cs
@@ -2,8 +2,8 @@
 
 Gameplay gameplay = new Gameplay();
 
-string[,] moves = new string[3, 3] { { " ", " ", " " }, { " ", " ", " " }, { " ", " ", " " } };// Create a game board array to store the players' choices.
-string board = "";
+//Sets up game board array and other variables to be used throughout.
+string[,] moves = new string[3, 3] { { " ", " ", " " }, { " ", " ", " " }, { " ", " ", " " } };
 int playerTurn = 1;
 int playerNumber = 1;
 bool validInput = false;
@@ -15,12 +15,14 @@ bool gameWon = false;
 // Welcome the user to the game.
 Console.WriteLine("Welcome to Tic Tac Toe");
 
-// Ask each player in turn for their choice and update the game board array.
+//Continues until someone wins the game or it ends in a tie.
 do
 {
+    //Sets up variables to store move position choices.
     int row;
     int col;
     
+    //Checks turn number to make sure the turn switches between players each time.
     if (playerTurn % 2 == 1) //If turn # is odd, player 1s turn
     {
         Console.WriteLine("Player 1, your turn!");
@@ -32,53 +34,60 @@ do
         playerNumber = 2;
     }
 
-
+    //Continues until the player has successfully made a move.
     do
     {
+        // Continues until a valid row is chosen.
         do
         {
             Console.WriteLine("Choose a row to play: 1, 2, or 3");
-            row = int.Parse(Console.ReadLine());
-            if (row < 1 || row > 3)
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out row) && (row >= 1 && row <= 3))
             {
-                Console.WriteLine("Invalid row number!");
-                validInput = false;
+                validInput = true; //Row must be an integer between 1 and 3.
             }
             else
             {
-                validInput = true;
+                Console.WriteLine("Invalid row number! Please enter 1, 2, or 3.");
+                validInput = false; 
             }
-        } while (validInput == false);
+        } while (!validInput);
 
+        // Continues until a valid column is chosen.
         do
         {
             Console.WriteLine("Choose a column to play: 1, 2, or 3");
-            col = int.Parse(Console.ReadLine());
-            if (col < 1 || col > 3)
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out col) && (col >= 1 && col <= 3))
             {
-                Console.WriteLine("Invalid column number!");
-                validInput = false;
+                validInput = true; //Column must be an integer between 1 and 3.
             }
             else
             {
-                validInput = true;
+                Console.WriteLine("Invalid column number! Please enter 1, 2, or 3.");
+                validInput = false;
             }
-        }while (validInput == false);
+        } while (!validInput);
 
+        //Checks whether the selected position is currently empty.
         if (moves[row-1,col-1] == " ")
         {
+            //If it's player 1's turn, fills the spot with an O.
             if (playerNumber == 1)
             {
                 moves[row-1, col-1] = "O";
             }
-
+            //If it's player 2's turn, fills the spot with an X.
             if (playerNumber == 2)
             {
                 moves[row-1, col-1] = "X";
             }
             
+            //Switches the played variable to true once a move has been made.
             played = true;
         }
+        //If the spot has already been filled, asks for a new position to be selected.
         else
         {
             Console.WriteLine("Invalid move, please try again.");
@@ -94,19 +103,21 @@ do
     if (gameplay.CheckForWinner(moves))
     {
         gameWon = true;
-        // and notify the players when a win has occurred and which player one the game.
+        //Tells the players who won the game based on who made the winning move.
         Console.WriteLine($"Congratulations! Player {playerNumber} has won the game!");
         gameOver = true;
     }
     if ((playerTurn == 9))
     {
-        // Bonus Note: should probably also have an option to account for a tie game.
+        //If the game board is full, ends the game with a "tie" message.
         Console.WriteLine("You tied.");
         gameOver = true;
     }
     
+    //Increments the player turn variable so that it'll be the next players turn on the next round.
     playerTurn++;
     
 } while (!gameOver);
 
+//Message to close out the game.
 Console.WriteLine("Game over, thanks for playing!");
