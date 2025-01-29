72bf3524c9070c4bc59f5b36b19215d7bf63d415
Harmony Tayler <153692183+harmonytayler@users.noreply.github.com>
2025-01-29 10:49:57 -0700

    Add project files.

---
+++ Gameplay.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission4_Assignment
+{
+    internal class Gameplay
+    {
+        // Receive the "board" array from the driver (Program) class.
+        
+        // Contain a method that prints the board based on the information passed to it.
+
+        // Contain a method that receives the game board array as input and returns if there is a winner and who it was.
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Mission4_Assignment;
+
+Gameplay gameplay = new Gameplay();
+
+// Welcome the user to the game.
+
+// Create a game board array to store the players' choices.
+
+// Ask each player in turn for their choice and update the game board array.
+
+// Print the board by calling the method in the supporting (Gameplay) class.
+
+// Check for a winner by calling the method in the supporting (Gameplay) class,
+// and notify the players when a win has occurred and which player one the game.
+    // Bonus Note: should probably also have an option to account for a tie game.
